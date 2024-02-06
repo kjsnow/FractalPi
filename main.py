2dc0e0f8ae49407a00208fffe2a28fba22844c7b
Kyle Snow <kyle.snow@opcity.com>
2024-02-05 21:29:55 -0600

    get async working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mandelbrot import Mandelbrot
 from PIL import Image as im
+import asyncio
 import numpy as np
 import glob
 import sys
@@ -9,7 +10,7 @@ import time
 DISPLAY_TYPE = "waveshare_epd.epd7in5_V2"
 
 # Disable when running the waveshare panel
-DEBUG = False
+DEBUG = True
 
 if not DEBUG:
     from omni_epd import displayfactory, EPDNotFoundError
@@ -48,38 +49,56 @@ def display_image(epd, image, debug=False):
         epd.sleep()
 
 
-def display_renders(epd, debug=False, sleep=10):
+async def display_renders(epd, debug=False, sleep=10):
     for file in sorted(glob.glob("renders/*.jpg")):
         with im.open(file) as image:
             display_image(epd, image, debug)
-            time.sleep(sleep)
+            await asyncio.sleep(sleep)
 
 
-def render_images(epd, debug=False, save_render=False):
+# @asyncio.coroutine
+async def render_images(epd, debug=False, save_render=False):
     render_iteration = 0
     while True:
+        print('I am a strange loop')
+        await asyncio.sleep(3)
     # for render_iteration in range(0, 10):
-        print(f"Starting render {render_iteration}...")
-        mandelbrot.render(WIDTH, HEIGHT)
-        print("Done!")
-        arr = mandelbrot.get_render()
-        arr = (np.asarray(arr)*255).astype(np.uint8)
-        image = im.fromarray(arr)
-        # Save the image as BMP
-        image = image.convert("1")
+    #     print(f"Starting render {render_iteration}...")
+    #     mandelbrot.render(WIDTH, HEIGHT)
+    #     print("Done!")
+    #     arr = mandelbrot.get_render()
+    #     arr = (np.asarray(arr)*255).astype(np.uint8)
+    #     image = im.fromarray(arr)
+    #     # Save the image as BMP
+    #     image = image.convert("1")
+    #
+    #     if save_render:
+    #         # Save to results
+    #         image.save(f"./renders/render_{render_iteration}.jpg")
+    #
+    #     display_image(epd, image, debug)
+    #
+    #     mandelbrot.zoom_on_interesting_area()
+    #     render_iteration += 1
 
-        if save_render:
-            # Save to results
-            image.save(f"./renders/render_{render_iteration}.jpg")
 
-        display_image(epd, image, debug)
 
-        mandelbrot.zoom_on_interesting_area()
-        render_iteration += 1
+
+async def main():
+    # 1 - THIS WORKS
+    render = asyncio.create_task(render_images(epd, True, False))
+    display = asyncio.create_task(display_renders(epd, debug=True, sleep=5))
+    await render
+    await display
+
+    # 2
+    # await asyncio.gather(render_images(epd, True, False), display_renders(epd, debug=True, sleep=5))
 
 
 if __name__ == "__main__":
     epd = prepare_display(width=WIDTH, height=HEIGHT, debug=DEBUG)
 
-    render_images(epd, debug=DEBUG, save_render=False)
+    # render_images(epd, debug=DEBUG, save_render=False)
     # display_renders(epd, DEBUG, 10)
+
+    asyncio.run(main())
